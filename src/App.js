0b1747d849dfafe458558c7569f0c46418d292dc
Leo Le <lnle125@gmail.com>
2020-07-26 14:34:42 -0700

    correction to allow for deployment to gh pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
-import Portfolio from './components/portfolio/portfolio.js';
-import About from './components/about/about.js';
-import Contact from "./components/contact/index.js"
+import Portfolio from "./components/portfolio/portfolio.js";
+import About from "./components/about/about.js";
+import Contact from "./components/contact/index.js";
 
-import './assets/utilities.css' 
-import './App.css';
+import "./assets/utilities.css";
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <BrowserRouter basename={window.location.origin}>
-                <Route default exact path="/" ><About/> </Route>
-                <Route exact path="/portfolio"> <Portfolio/> </Route>
-                <Route exact path="/contact"> <Contact/> </Route>
-            </BrowserRouter>
-            <Footer />
-        </div>
+  return (
+    <div className="App">
+      <Navbar />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route default exact path="/">
+          <About />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
